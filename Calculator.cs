e68956994ab9f9577b863bd76cd01aed05884125
Nyxelll <fakhridwiokta@students.amikom.ac.id>
2025-03-12 14:10:22 +0700

    penambahan fungsi

--- Calculator.cs
+++ Calculator.cs
@@ -6,7 +6,21 @@
 
 
         }
-        
+        static float Penambahan (float a,float b) {
+            return a + b;
+              }
+        static float Pengurangan(float a, float b)
+        {
+            return a - b;
+        }
+        static float Perkalian(float a, float b)
+        {
+            return a * b;
+        }
+        static float Pembagian(float a, float b)
+        {
+            return a / b;
+        }
         
     
 }
