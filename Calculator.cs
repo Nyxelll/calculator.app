ee6c734ea6b567c61bbdd8b535cdf8eed971c242
Nyxelll <fakhridwiokta@students.amikom.ac.id>
2025-03-12 14:12:07 +0700

    penambahan fungsi main

--- Calculator.cs
+++ Calculator.cs
@@ -2,7 +2,21 @@
     {
         static void Main(string[] args)
         {
-           
+             Console.Title = "Aplikasi Kalkulator ";
+            Console.Write("inputkan nilai a = ");
+            float a =float.Parse(Console.ReadLine());
+            Console.Write("inputkan nilai b = ");
+            float b = Convert.ToSingle(Console.ReadLine()); 
+
+            Console.WriteLine("selamat datang di kalkulator fahri ");
+            Console.WriteLine("Hasil Penambahan "+a+"+"+b+"=" +Penambahan (a,b));
+            Console.WriteLine("hasil Pengurangan {0} - {1} = {2} ",a,b,Pengurangan (a,b));
+            Console.WriteLine("hasil Perkalian {0} * {1} = {2} ", a, b, Perkalian(a, b));
+            Console.WriteLine("hasil Pembagian {0} / {1} = {2} ", a, b, Pembagian(a, b));
+
+            Console.WriteLine("\n Tekan sembarang key untuk keluar ");
+            Console.ReadKey();
+
 
 
         }
